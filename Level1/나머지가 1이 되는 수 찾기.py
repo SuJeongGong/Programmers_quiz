cdd46331be7ab193a9d2e31974a07bb74497313d
공수정 <0_sujeong@naver.com>
2022-05-13 12:24:29 +0900

    2022.05.13 레벨1 문제풀기

---
+++ Level1/나머지가 1이 되는 수 찾기.py
@@ -0,0 +1,19 @@
+import time
+
+
+def mianJob(n):
+    start = time.time()
+    print(f"[time]\t{time.time()-start:.5f}\t [answer] : {solution(n)}")
+    return
+
+
+def solution(n):
+    for x in range(1, n + 1):
+        if n % x == 1:
+            if
+            return x
+
+
+if __name__ == "__main__":
+    for i in range(3, 1000001):
+        mianJob(i)
