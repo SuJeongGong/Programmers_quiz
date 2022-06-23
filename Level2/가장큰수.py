9d31e40991192842ccf2a094df45ca6cf4ce8a30
공수정 <0sujeong@nectarsoft.co.kr>
2022-06-23 11:25:07 +0900

    2022.06.23 가장 큰 수 푸는중

--- Level2/가장큰수.py
+++ Level2/가장큰수.py
@@ -1,17 +1,29 @@
 import time
+from itertools import *
 
 
 def mianJob(numbers):
-    start = time.time()
-    print(f"[time]\t{time.time()-start:.5f}\t [answer] : {solution(numbers)}")
+    start = time.process_time()
+    result = solution(numbers)
+    end = time.process_time()
+
+    print(f"[time]\t{int(round((end - start) * 1000))}ms\t [answer] : {result}")
     return
 
 
 def solution(numbers):
-
+    # sum = []
+    # for x in permutations([str(y) for y in numbers], len(numbers)):
+    #     sum.append(int("".join(x)))
+    #
+    # return str(max(sum))
+    # return "".join(sorted([str(x) for x in numbers], key=lambda x: (x[0]))[::-1])
+    # return "".join(sorted([str(x) for x in numbers], key=lambda x: (-int(x[0]))))
+    # return "".join(sorted([str(x) for x in numbers], key=lambda x:(x[y] for y in len(x)) if(len(x)>1) else x[0])[::-1])
 
 
 
 if __name__ == "__main__":
     mianJob([6, 10, 2])
     mianJob([3, 30, 34, 5, 9])
+    mianJob([3, 30, 34, 5, 9, 30, 34, 5, 9, 30, 34, 5, 9, 30, 34, 5, 9, 30, 34, 5, 9, 30, 34, 5, 9, 30, 34, 5, 9, 30, 34, 5, 9, 30, 34, 5, 9, 30, 34, 5, 9, 30, 34, 5, 9, 30, 34, 5, 9, 30, 34, 5, 9, 30, 34, 5, 9, 30, 34, 5, 9, 30, 34, 5, 9, 30, 34, 5, 9])
