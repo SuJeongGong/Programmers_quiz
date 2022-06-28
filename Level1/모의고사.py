820ee270d385f6b9e6d8f04adc6376ad95bf8e18
공수정 <0sujeong@nectarsoft.co.kr>
2022-06-28 14:44:34 +0900

    2022.06.27 모의고사 완료

--- Level1/모의고사.py
+++ Level1/모의고사.py
@@ -9,21 +9,17 @@ def mianJob(answers):
 def solution(answers):
 
     check = [[1, 2, 3, 4, 5],[2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
-    # result = {1:0, 2:0, 3:0}
     result =[0,0,0]
 
     for i in range(len(answers)):
         if check[0][i % 5] == answers[i]:
-            # result[1] += 1
             result[0] += 1
-        if ((i+1) % 2 == 1 and answers[i] == 2) or ((i+1) % 2 == 0 and check[1][(i+1) // 2 % 4] == answers[i]):
-            # result[0] += 1
+        if check[1][i % 8] == answers[i]:
+            result[1] += 1
+        if check[2][i % 10] == answers[i]:
             result[2] += 1
-        if check[2][(i+1) % 10] == answers[i]:
-            # result[3] += 1
-            result[0] += 1
 
-    return result.index(max(result))
+    return sorted([i + 1  for i, v in enumerate(result) if v == max(result)])
 
 
 if __name__ == "__main__":
